958a52b63693fdbfc64023ed97527266bdec0c66
NITHISHKUMAR.T <151352721+NITHISHKUMR1@users.noreply.github.com>
2024-07-31 21:25:47 +0530

    Create largest number .py

---
+++ 31.07.2024/largest number .py
@@ -0,0 +1,11 @@
+def n(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+print(f"The largest number is {n(num1, num2, num3)}.")
